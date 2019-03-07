7692b26b06aa4fe21725c6c687a286a3449bb808
Ibrahim <creativeibi77@gmail.com>
2019-03-07 14:36:13 +1100

    some untested test files

---
+++ python-test.py
@@ -0,0 +1,16 @@
+from time import sleep
+import turtle
+
+#to run open file in terminal using python
+
+t = turtle.Pen()
+for i in range(8):
+    break
+    for n in range(4):
+        t.forward(100)
+        sleep(0.1)
+        t.left(90)
+        sleep(0.1)
+    sleep(0.2)
+    t.right(45)
+sleep(1)
