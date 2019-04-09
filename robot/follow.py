82aea58ac1305526dbc6aaf461330e35a60929f2
Ibrahim <creativeibi77@gmail.com>
2019-04-09 15:35:40 +1000

    switched to blob tracking

--- robot/follow.py
+++ robot/follow.py
@@ -28,18 +28,18 @@ print("1" + str(time.process_time()), file=sys.stderr)
 # take first frame of the video
 ret,frame = cap.read()
 
-# setup initial location of window
-r,h,c,w = 250,90,400,125  # simply hardcoded the values
-global track_window
-track_window = (c,r,w,h)
-# set up the ROI for tracking
-roi = frame[r:r+h, c:c+w]
-hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
-mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
-roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
-cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
-# Setup the termination criteria, either 10 iteration or move by atleast 1 pt
-term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
+# # setup initial location of window
+# r,h,c,w = 250,90,400,125  # simply hardcoded the values
+# global track_window
+# track_window = (c,r,w,h)
+# # set up the ROI for tracking
+# roi = frame[r:r+h, c:c+w]
+# hsv_roi =  cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+# mask = cv.inRange(hsv_roi, np.array((0., 60.,32.)), np.array((180.,255.,255.)))
+# roi_hist = cv.calcHist([hsv_roi],[0],mask,[180],[0,180])
+# cv.normalize(roi_hist,roi_hist,0,255,cv.NORM_MINMAX)
+# # Setup the termination criteria, either 10 iteration or move by atleast 1 pt
+# term_crit = ( cv.TERM_CRITERIA_EPS | cv.TERM_CRITERIA_COUNT, 10, 1 )
 
 def take_pic():
     global track_window
@@ -58,25 +58,37 @@ def take_pic():
         mask1 = cv.inRange(hsv, zero, lower)
         mask2 = cv.inRange(hsv, upper, biggest)
         mask = cv.add(mask1, mask2)
+
+        M = cv.moments(mask)
+
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+        else:
+            cX, cY = 0, 0
+
         # apply meanshift to get the new location
-        ret, track_window = cv.CamShift(mask, track_window, term_crit)
-        # Draw it on image
-        pts = cv.boxPoints(ret)
-        pts = np.int0(pts)
+        # ret, track_window = cv.CamShift(mask, track_window, term_crit)
+        # # Draw it on image
+        # pts = cv.boxPoints(ret)
+        # pts = np.int0(pts)
 
-        saved_image = np.zeros((480,640,3), np.uint8)
-        img2 = cv.polylines(saved_image,[pts],True, 255,5)
+        # saved_image = np.zeros((480,640,3), np.uint8)
+        # saved_image[:,:]=(255,255,255)
 
-        avg = avg_4([pts])
-        img2 = cv.circle(saved_image, avg, 10, 255, -1)
+        # saved_image = cv.polylines(saved_image,[pts],True, 255,8)
 
-        cv.imwrite("disp.png",saved_image)
+        # avg = avg_4([pts])
+        # saved_image = cv.circle(saved_image, (int(avg[0]),int(avg[1])), 24, 255, -1)
 
-        print([pts], file=sys.stderr)
-        print(avg_4([pts]), file=sys.stderr)
+        # cv.imwrite("disp.png",saved_image)
+
+        # print([pts], file=sys.stderr)
+        # print(avg_4([pts]), file=sys.stderr)
+
         print("end snap" + str(time.process_time()), file=sys.stderr)
 
-        return avg[0]
+        return cX
 
     else:
         return False
@@ -96,10 +108,13 @@ while True:
 
     print(str(x_ratio) +", "+ str(xpos) + " @ " + str(time.process_time()), file=sys.stderr)
 
-    # lcd.circle(clear_screen=True, x=x_ratio*178, y=64, radius=10, fill_color='lightgrey')
+    lcd.clear()
+
+    lcd.circle(clear_screen=True, x=x_ratio*178, y=64, radius=10, fill_color='lightgrey')
     
-    img = Image.open('disp.png')
-    lcd.image.paste(img, (0,0))
+    # img = Image.open('disp.png')
+    # lcd.image.paste(img, (0,0))
+
     lcd.update()
 
     # if abs(x_ratio - 1) < 0.2: #can go straight
